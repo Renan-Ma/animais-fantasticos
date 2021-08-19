688c358e42e15fae5edc4348b1bdeed4d2c43206
Renan Martinez <renan_martinez89@hotmail.com>
2021-08-19 18:15:54 -0300

    add fetch-bitcoin

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ import iniDropdownMenu from './modules/dropdown-menu.js';
 import iniMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
+import initFetchBitcoin from './modules/fetch-bitcoin.js';
 
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
@@ -25,9 +26,10 @@ modal.init();
 const tooltip = new Tooltip('[data-tooltip]');
 tooltip.init();
 
-initAnimacaoScroll();
+fetchAnimais('../../animaisapi.json', '.numeros-grid');
 
+initAnimacaoScroll();
 iniDropdownMenu();
 iniMenuMobile();
 initFuncionamento();
-fetchAnimais('../../animaisapi.json', '.numeros-grid');
+initFetchBitcoin();
